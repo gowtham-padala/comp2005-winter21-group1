0238c1d9e25e217e51c86384c581a54b925e41cc
Masoud Zare <57842159+masoudz88@users.noreply.github.com>
2021-03-30 19:03:26 -0230

    Add files via upload

---
+++ Group 1 Iteration 4/Malefiz/src/NewGameWin.java
@@ -0,0 +1,298 @@
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class NewGameWin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField name_1;
+	private JTextField name_2;
+	private JTextField name_3;
+	private JTextField name_4;
+	private String players_details;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+					NewGameWin frame = new NewGameWin();
+					frame.setVisible(true);
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public NewGameWin() {
+		setResizable(false);
+		setTitle("New Game");
+		setBounds(100, 100, 410, 390);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JLabel main_title = new JLabel("Players");
+		main_title.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		main_title.setBounds(10, 10, 69, 19);
+		getContentPane().add(main_title);
+		
+		JLabel choice_title = new JLabel("Choice");
+		choice_title.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		choice_title.setBounds(20, 39, 45, 13);
+		getContentPane().add(choice_title);
+		
+		
+		JComboBox type_1 = new JComboBox();
+		type_1.setEnabled(false);
+		type_1.setModel(new DefaultComboBoxModel(new String[] {"Human", "System"}));
+		type_1.setSelectedIndex(0);
+		type_1.setBounds(117, 69, 77, 21);
+		getContentPane().add(type_1);
+		
+		JComboBox type_2 = new JComboBox();
+
+		type_2.setModel(new DefaultComboBoxModel(new String[] {"Human", "System"}));
+		type_2.setSelectedIndex(1);
+		type_2.setBounds(117, 115, 77, 21);
+		getContentPane().add(type_2);
+		
+
+		
+		JComboBox type_3 = new JComboBox();
+		type_3.setEnabled(false);
+		type_3.setModel(new DefaultComboBoxModel(new String[] {"Human", "System"}));
+		type_3.setBounds(117, 161, 77, 21);
+		getContentPane().add(type_3);
+		
+		JComboBox type_4 = new JComboBox();
+		type_4.setEnabled(false);
+		type_4.setModel(new DefaultComboBoxModel(new String[] {"Human", "System"}));
+		type_4.setToolTipText("");
+		type_4.setBounds(117, 207, 77, 21);
+		getContentPane().add(type_4);
+		
+		JLabel name_title = new JLabel("Player Name");
+		name_title.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		name_title.setBounds(223, 39, 78, 13);
+		getContentPane().add(name_title);
+		
+		name_1 = new JTextField();
+		name_1.setText("Human 1");
+		name_1.setBounds(210, 69, 96, 19);
+		getContentPane().add(name_1);
+		name_1.setColumns(10);
+		
+		name_2 = new JTextField();
+		name_2.setText("Computer 1");
+		name_2.setBounds(210, 115, 96, 19);
+		getContentPane().add(name_2);
+		name_2.setColumns(10);
+		
+		name_3 = new JTextField();
+		name_3.setEnabled(false);
+		name_3.setBounds(210, 161, 96, 19);
+		getContentPane().add(name_3);
+		name_3.setColumns(10);
+		
+		name_4 = new JTextField();
+		name_4.setEnabled(false);
+		name_4.setBounds(210, 207, 96, 19);
+		getContentPane().add(name_4);
+		name_4.setColumns(10);
+		
+		JLabel level_title = new JLabel("Level");
+		level_title.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		level_title.setBounds(329, 39, 38, 13);
+		getContentPane().add(level_title);
+		
+		JComboBox level_1 = new JComboBox();
+		level_1.setEnabled(false);
+		level_1.setModel(new DefaultComboBoxModel(new String[] {"1", "2"}));
+		level_1.setBounds(327, 69, 38, 21);
+		getContentPane().add(level_1);
+		
+		JComboBox level_2 = new JComboBox();
+		level_2.setModel(new DefaultComboBoxModel(new String[] {"1", "2"}));
+		level_2.setBounds(327, 115, 38, 21);
+		getContentPane().add(level_2);
+		
+		JComboBox level_3 = new JComboBox();
+		level_3.setEnabled(false);
+		level_3.setModel(new DefaultComboBoxModel(new String[] {"1", "2"}));
+		level_3.setBounds(327, 161, 38, 21);
+		getContentPane().add(level_3);
+		
+		JComboBox level_4 = new JComboBox();
+		level_4.setEnabled(false);
+		level_4.setModel(new DefaultComboBoxModel(new String[] {"1", "2"}));
+		level_4.setBounds(327, 207, 38, 21);
+		getContentPane().add(level_4);
+		
+		JCheckBox choice_box_1 = new JCheckBox("");
+		choice_box_1.setEnabled(false);
+		choice_box_1.setSelected(true);
+		choice_box_1.setBounds(32, 69, 21, 21);
+		getContentPane().add(choice_box_1);
+		
+		JCheckBox choice_box_2 = new JCheckBox("");
+		choice_box_2.setEnabled(false);
+		choice_box_2.setSelected(true);
+		choice_box_2.setBounds(32, 115, 21, 21);
+		getContentPane().add(choice_box_2);
+		
+		JCheckBox choice_box_3 = new JCheckBox("");
+		choice_box_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(choice_box_3.isSelected()){
+					type_3.setEnabled(true);
+					name_3.setEnabled(true);
+				}
+				else {
+					type_3.setEnabled(false);
+					name_3.setEnabled(false);
+					level_3.setEnabled(false);
+				}
+			}
+		});
+		choice_box_3.setBounds(32, 161, 21, 21);
+		getContentPane().add(choice_box_3);
+		
+		JCheckBox choice_box_4 = new JCheckBox("");
+		choice_box_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(choice_box_4.isSelected()){
+					type_4.setEnabled(true);
+					name_4.setEnabled(true);
+				}
+				else {
+					type_4.setEnabled(false);
+					name_4.setEnabled(false);
+					level_4.setEnabled(false);
+				}
+			}
+		});
+		choice_box_4.setBounds(32, 207, 21, 21);
+		getContentPane().add(choice_box_4);
+		
+		JLabel color_title = new JLabel("Color");
+		color_title.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		color_title.setBounds(75, 40, 38, 13);
+		getContentPane().add(color_title);
+		
+		Canvas color_1 = new Canvas();
+		color_1.setBackground(Color.RED);
+		color_1.setBounds(82, 73, 14, 13);
+		getContentPane().add(color_1);
+		
+		Canvas color_2 = new Canvas();
+		color_2.setBackground(Color.BLUE);
+		color_2.setBounds(82, 118, 14, 13);
+		getContentPane().add(color_2);
+		
+		Canvas color_3 = new Canvas();
+		color_3.setBackground(Color.YELLOW);
+		color_3.setBounds(82, 164, 14, 13);
+		getContentPane().add(color_3);
+		
+		Canvas color_4 = new Canvas();
+		color_4.setBackground(Color.GREEN);
+		color_4.setBounds(82, 210, 14, 13);
+		getContentPane().add(color_4);
+		
+		JLabel type_title = new JLabel("Type");
+		type_title.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		type_title.setBounds(140, 39, 45, 13);
+		getContentPane().add(type_title);
+		
+		type_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(type_2.getSelectedIndex()==0){
+					level_2.setEnabled(false);
+				}
+				else {
+					level_2.setEnabled(true);
+				}
+			}
+		});
+		
+		type_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(type_3.getSelectedIndex()==0){
+					level_3.setEnabled(false);
+				}
+				else {
+					level_3.setEnabled(true);
+				}
+			}
+		});
+		
+		type_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(type_4.getSelectedIndex()==0){
+					level_4.setEnabled(false);
+				}
+				else {
+					level_4.setEnabled(true);
+				}
+			}
+		});
+
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 265, 376, 2);
+		getContentPane().add(separator);
+		
+
+		
+		
+		JButton start_button = new JButton("Next");
+		start_button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				players_details = "1 "+ type_1.getSelectedItem() + " " + name_1.getText() + " " + level_1.getSelectedItem() + "\n";
+				players_details = players_details + "2 "+ type_2.getSelectedItem() + " " + name_2.getText() + " " + level_2.getSelectedItem()  + "\n";
+				
+				if(choice_box_3.isSelected()) {
+					players_details = players_details + "3 "+ type_3.getSelectedItem() + " " + name_3.getText() + " " + level_3.getSelectedItem()  + "\n" ;			
+				}
+				
+				if(choice_box_4.isSelected()) {
+					players_details = players_details + "4 "+ type_4.getSelectedItem() + " " + name_4.getText() + " " + level_4.getSelectedItem()  + "\n";			
+				} 
+				
+				System.out.println(players_details);
+				
+                dispose();
+                DisplayOptions dp = new DisplayOptions();
+                dp.setVisible(true);
+			}
+		});
+		start_button.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		start_button.setBounds(49, 293, 129, 32);
+		getContentPane().add(start_button);
+		
+		JButton cancel_button = new JButton("Cancel");
+		cancel_button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		cancel_button.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		cancel_button.setBounds(213, 293, 129, 32);
+		getContentPane().add(cancel_button);
+	}
+
+}
---
+++ Group 1 Iteration 4/Malefiz/src/mainBoard.java
@@ -0,0 +1,223 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.math.*;
+
+public class mainBoard extends JFrame{
+	private int turn = 0;
+	private int display;
+	private JPanel top_panel;
+	private JPanel centre_panel;
+	private JPanel bottom_panel;
+	private JButton [][] squares=new JButton[17][17];
+	private int row=15;
+	private int col=2;
+	private int[][] map;
+	private int randint;
+
+	private ImageIcon pawn=new ImageIcon("./pawn.jpg");
+	private ImageIcon dices=createImageIcon("images/dice_1.png");
+
+	public mainBoard()
+	{
+
+		Container contentPane = getContentPane();
+		setBounds(100, 100, 600, 600);
+		contentPane.setLayout(new BorderLayout());
+		top_panel=new JPanel();
+		centre_panel=new JPanel();
+		centre_panel.setLayout(new GridLayout());
+		bottom_panel=new JPanel();
+		contentPane.add(top_panel, BorderLayout.NORTH);
+		contentPane.add(centre_panel, BorderLayout.CENTER);
+		contentPane.add(bottom_panel,BorderLayout.SOUTH);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu menu = new JMenu("File");
+		menuBar.add(menu);
+		JMenuItem item1 = new JMenuItem("New Game");
+           item1.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    dispose();
+                    NewGameWin ng = new NewGameWin();
+                    ng.setVisible(true);
+                }
+            });
+		menu.add(item1);
+
+		JMenuItem item2 = new JMenuItem("Save Game");
+		menu.add(item2);
+
+		JMenuItem item3 = new JMenuItem("Load Game");
+		menu.add(item3);
+
+		JMenuItem item4 = new JMenuItem("Help");
+            item4.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    help hp = new help();
+                    hp.setVisible(true);
+                }
+            });
+		menu.add(item4);
+
+		JMenuItem item5 = new JMenuItem("Quit Game");
+		menu.add(item5);
+		JLabel playerTurnLabel=new JLabel("Player 1's turn");
+		top_panel.add(playerTurnLabel);
+
+		centre_panel.setLayout(new GridLayout(17,17));
+		int[][] map = {
+				{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+				{1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1},
+				{0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,1,1,3,1,1,0,0,0,0,0,0},
+				{0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0},
+				{0,0,0,0,1,1,3,1,1,1,3,1,1,0,0,0,0},
+				{0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0},
+				{0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0},
+				{0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0},
+				{3,1,1,1,3,1,1,1,3,1,1,1,3,1,1,1,3},
+				{1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{0,0,2,0,0,0,2,0,0,0,2,0,0,0,2,0,0},
+				{0,2,2,2,0,2,2,2,0,2,2,2,0,2,2,2,0},
+				{0,0,2,0,0,0,2,0,0,0,2,0,0,0,2,0,0}
+		};
+		for(int i = 0; i < 17; i++) {
+			for (int j = 0; j < 17; j++) {
+				if (map[i][j]==1)
+				{
+					squares[i][j] = new JButton();
+					squares[i][j].setBackground(Color.PINK);
+					centre_panel.add(squares[i][j]);
+					int finalI = i;
+					int finalJ = j;
+					squares[i][j].addActionListener(e -> {
+						processClick(finalI, finalJ);
+						return;
+					});
+				}
+				else if (map[i][j]==0)
+				{
+					squares[i][j] = new JButton();
+					squares[i][j].setBackground(Color.BLACK);
+					squares[i][j].setEnabled(false);
+					centre_panel.add(squares[i][j]);
+				}
+				else if (map[i][j]==2)
+				{
+					squares[i][j] = new JButton();
+					squares[i][j].setBackground(Color.GREEN);
+					centre_panel.add(squares[i][j]);
+				}
+				else
+				{
+					squares[i][j] = new JButton();
+					squares[i][j].setBackground(Color.GRAY);
+					centre_panel.add(squares[i][j]);
+				}
+			}
+		}
+		squares[row][col].setBackground(Color.red);
+
+		bottom_panel.setLayout(new FlowLayout());
+
+		JButton rollBtn = new JButton("Roll");
+
+		rollBtn.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		rollBtn.setBounds(5, 26, 60, 26);
+		bottom_panel.add(rollBtn);
+
+		JLabel p1l1 = new JLabel("Player 1\r\n\r\n");
+		p1l1.setForeground(Color.RED);
+		p1l1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		p1l1.setBounds(106, 30, 100, 21);
+		//bottom_panel.add(p1l1);
+
+		JLabel p2l2 = new JLabel("Player 2\r\n\r\n");
+		p2l2.setForeground(Color.BLUE);
+		p2l2.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		p2l2.setBounds(226, 30, 100, 21);
+		//bottom_panel.add(p2l2);
+
+		JLabel p3l3 = new JLabel("Player 3\r\n\r\n");
+		p3l3.setForeground(Color.YELLOW);
+		p3l3.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		p3l3.setBounds(342, 30, 100, 21);
+		//bottom_panel.add(p3l3);
+
+		JLabel p4l4 = new JLabel("Player 4\r\n\r\n");
+		p4l4.setForeground(Color.GREEN);
+		p4l4.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		p4l4.setBounds(458, 30, 100, 21);
+		//bottom_panel.add(p4l4);
+		
+		JLabel diceimg = new JLabel();
+		diceimg.setIcon(dices);
+		bottom_panel.add(diceimg);
+		rollBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Random rand = new Random();
+				randint = rand.nextInt(6)+1;
+				ImageIcon dices= new ImageIcon("images/dice_"+randint+".png");
+				diceimg.setIcon(dices);
+				bottom_panel.add(diceimg);
+				turn = turn + 1;
+				display = turn%4+1;
+				playerTurnLabel.setText("Player "+ display + "'s turn");
+			}
+		});
+		bottom_panel.setBackground(Color.darkGray);
+		setVisible(true);
+		setResizable(false);
+		setLocationRelativeTo(null);//shows the window on centre
+	}
+	public boolean validMovements(int i, int j){
+
+		int validRow=Math.abs(i-row);
+		int validCol=Math.abs(i-row);
+		
+		if (validRow==1 ){
+			return true;
+		}
+		if (validCol==1){
+			return true;
+		}
+		return false;
+
+	}
+	public void processClick(int i, int j){
+		/*if(validMovements(i,j)==false){
+			return;
+		}*/
+		squares[row][col].setBackground(Color.PINK);
+		squares[i][j].setBackground(Color.red);
+		row=i;
+		col=j;
+	}
+	private ImageIcon createImageIcon(final String name) {
+        try {
+            BufferedImage pictureID = ImageIO.read(new File(name));
+            ImageIcon icon = new ImageIcon(pictureID);
+            return icon;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+}
+
+
