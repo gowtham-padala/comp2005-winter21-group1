31e5e8e5dfcb00d58819dba958aba8b57d39e5f0
masoudz88 <masoudz@mun.ca>
2021-04-04 19:55:13 -0230

    set player names

--- Group 1 Iteration 4/Malefiz/src/NewGameWin.java
+++ Group 1 Iteration 4/Malefiz/src/NewGameWin.java
@@ -93,12 +93,6 @@ public class NewGameWin extends JFrame {
 
 		name_1 = new JTextField();
 		name_1.setText("Human 1");
-		name_1.addActionListener(e->{
-			String input=name_1.getText();
-			player_1=new Player(input,Color.red);
-		});
-		player_1=new Player(name_1.getText(),Color.red);
-		players_attending_game.add(player_1);
 		name_1.setBounds(210, 69, 96, 19);
 		getContentPane().add(name_1);
 
@@ -106,8 +100,6 @@ public class NewGameWin extends JFrame {
 
 		name_2 = new JTextField();
 		name_2.setText("Human 2");
-		player_2=new Player(name_2.getText(),Color.blue);
-		players_attending_game.add(player_2);
 		name_2.setBounds(210, 115, 96, 19);
 		getContentPane().add(name_2);
 		name_2.setColumns(10);
@@ -115,8 +107,6 @@ public class NewGameWin extends JFrame {
 		name_3 = new JTextField();
 		name_3.setText("Human 3");
 		name_3.setEnabled(false);
-		player_3=new Player(name_3.getText(),Color.yellow);
-		players_attending_game.add(player_3);
 		name_3.setBounds(210, 161, 96, 19);
 		getContentPane().add(name_3);
 		name_3.setColumns(10);
@@ -124,8 +114,6 @@ public class NewGameWin extends JFrame {
 		name_4 = new JTextField();
 		name_4.setText("Human 4");
 		name_4.setEnabled(false);
-		player_4=new Player(name_4.getText(),Color.green);
-		players_attending_game.add(player_4);
 		name_4.setBounds(210, 207, 96, 19);
 		getContentPane().add(name_4);
 		name_4.setColumns(10);
@@ -294,7 +282,14 @@ public class NewGameWin extends JFrame {
 
 				System.out.println(players_details);
 				System.out.println(players_attending_game.toString());
-
+				player_1=new Player(name_1.getText(),Color.red);
+				players_attending_game.add(player_1);
+				player_2=new Player(name_2.getText(),Color.blue);
+				players_attending_game.add(player_2);
+				player_3=new Player(name_3.getText(),Color.yellow);
+				players_attending_game.add(player_3);
+				player_4=new Player(name_4.getText(),Color.green);
+				players_attending_game.add(player_4);
 				dispose();
 				DisplayOptions dp = new DisplayOptions(players_attending_game);
 				dp.setVisible(true);
@@ -314,5 +309,6 @@ public class NewGameWin extends JFrame {
 		cancel_button.setBounds(213, 293, 129, 32);
 		getContentPane().add(cancel_button);
 	}
+
 
 }
